bdac123d27a2b75d1f198ca9af24f5dc498f501d
yhkim <kvoyant@gmail.com>
2019-05-12 15:21:32 +0900

    내 정보 가져오기 API 초안 작성

--- app/src/main/java/com/tjeit/loginserverconnectpractice0512/utils/ConnectServer.java
+++ app/src/main/java/com/tjeit/loginserverconnectpractice0512/utils/ConnectServer.java
@@ -11,6 +11,7 @@ import java.io.IOException;
 import okhttp3.Call;
 import okhttp3.Callback;
 import okhttp3.FormBody;
+import okhttp3.HttpUrl;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.RequestBody;
@@ -25,7 +26,7 @@ public class ConnectServer {
         void onResponse(JSONObject json);
     }
 
-    public static void postRequestSignIn(Context context, String user_id, String password , final JsonResponsHandler handler) {
+    public static void postRequestSignIn(Context context, String user_id, String password, final JsonResponsHandler handler) {
 
 //        클라이언트 역할은 동일
         OkHttpClient client = new OkHttpClient();
@@ -40,7 +41,7 @@ public class ConnectServer {
 //        실제 Request를 생성, 서버로 떠날 준비.
 
         Request request = new Request.Builder()
-                .url(BASE_URL+"/auth")
+                .url(BASE_URL + "/auth")
                 .post(requestBody)
                 .build();
 
@@ -70,9 +71,56 @@ public class ConnectServer {
             }
         });
 
+    }
 
+    //필요한 서버의 요청들을 하나하나 메쏘드로 만들어 주자.
+    public static void getRequestMeInfo(Context context, String token, final JsonResponsHandler handler) {
 
+//      서버 - 클라이언트(앱)
+        OkHttpClient client = new OkHttpClient();
 
+//        URL 설정 => 목적지 설정
+        HttpUrl.Builder urlBuilder = HttpUrl.parse(BASE_URL + "/v2/me_info").newBuilder();
+
+//        ※ GET, DELETE 메쏘드는 필요 파라미터를 URL에 담아 줘야 함. 이 담는 과정을 쉽게 하려고 urlBuilder 사용.
+
+//        실제로 서버에 접근하는 완성된 URL
+        String requestURL = urlBuilder.build().toString();
+
+//        완성된 URL로 접근하는 request 생성.
+        Request request = new Request.Builder()
+                .header("X-Http-Token", token)
+                .url(requestURL)
+                .build();
+
+//        만들어진 Request를 실제로 서버에 요청
+        client.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                String reponseContent = response.body().string();
+
+                Log.d("서버 응답 내용", reponseContent);
+
+                try {
+                    //받아온 응답을 JSON객체로 변환
+                    JSONObject json = new JSONObject(reponseContent);
+
+                    if (handler != null) {
+                        handler.onResponse(json);
+                    }
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
 
     }
+
+
 }
