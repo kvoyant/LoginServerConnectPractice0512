8ad8072a7356b3d70ec54de76240b8b0fb44f2f3
yhkim <kvoyant@gmail.com>
2019-05-12 12:19:27 +0900

    로그인화면 그리기 - 수정

--- app/src/main/java/com/tjeit/loginserverconnectpractice0512/LoginActivity.java
+++ app/src/main/java/com/tjeit/loginserverconnectpractice0512/LoginActivity.java
@@ -3,6 +3,7 @@ package com.tjeit.loginserverconnectpractice0512;
 import android.databinding.DataBindingUtil;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 import com.tjeit.loginserverconnectpractice0512.databinding.ActivityMainBinding;
 
@@ -23,7 +24,14 @@ public class LoginActivity extends BaseActivity {
 
     @Override
     public void setupEvents() {
-
+        act.loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+//                1.아이디와 비번 입력을 받아오기
+//                2.받아온 아이디와 비번이 정말로 올바른 회원인지? 검사
+//                아이디 비번이 모두
+            }
+        });
     }
 
     @Override
